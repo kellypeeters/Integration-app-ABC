22f54a28e50ada216c5415a765a0eb33c4181ec2
KellyPeeters <kelly.peeters@student.ehb.be>
2019-05-20 11:42:13 +0200

    Accountpagina is klaar

--- Javascript/Basis.js
+++ Javascript/Basis.js
@@ -1,7 +1,5 @@
     $("#Registratieform").hide();
 
-    var attempt = 10;
-
     function validate() {
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
@@ -9,22 +7,17 @@
             window.location = "Dashboard.html";
             return false;
         } else {
-            attempt--;
-            if (attempt == 0) {
-                document.getElementById("username").disabled = true;
-                document.getElementById("password").disabled = true;
-                document.getElementById("submit").disabled = true;
-                return false;
-            }
+            alert("Email en wachtwoord komen niet overeen");
         }
-    } 
+    }   
+
 
     $(".GeenAccount").click(function () {
         $("#Loginform").hide();
         $("#Registratieform").show();
     });
 
-$(".AlAccount").click(function () {
-    $("#Registratieform").hide();
-    $("#Loginform").show(); 
-});  
+    $(".AlAccount").click(function () {
+        $("#Registratieform").hide();
+        $("#Loginform").show();
+    });
