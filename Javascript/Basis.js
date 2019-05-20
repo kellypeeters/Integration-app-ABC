edf7afd583987b11cfe3950a84069eca453b2e6a
KellyPeeters <kelly.peeters@student.ehb.be>
2019-05-20 11:03:30 +0200

    Accountpagina angepast

--- Javascript/Basis.js
+++ Javascript/Basis.js
@@ -1,23 +1,30 @@
-var attempt = 10; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username == "Kelly" && password == "Peeters") {
-        window.location = "Dashboard.html"; // Redirecting to other page.
-        return false;
-    } else {
-        attempt--; // Decrementing by one.
-        if (attempt == 0) {
-            document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("submit").disabled = true;
-            return false;
-        }
-    }
-}
+    $("#Registratieform").hide();
 
-$("#Registeratiebutton").click(function () {
-                $("#Loginform").hide();
-                $("#Registratieform").show(); 
-}
+    var attempt = 10;
+
+    function validate() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (username == "peeters.kelly@outlook.com" && password == "Peeters") {
+            window.location = "Dashboard.html";
+            return false;
+        } else {
+            attempt--;
+            if (attempt == 0) {
+                document.getElementById("username").disabled = true;
+                document.getElementById("password").disabled = true;
+                document.getElementById("submit").disabled = true;
+                return false;
+            }
+        }
+    } 
+
+    $(".GeenAccount").click(function () {
+        $("#Loginform").hide();
+        $("#Registratieform").show();
+    });
+
+$(".AlAccount").click(function () {
+    $("#Registratieform").hide();
+    $("#Loginform").show(); 
+});  
