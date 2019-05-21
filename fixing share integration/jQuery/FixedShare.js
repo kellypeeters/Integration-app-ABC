9bded2341fa6066d0d3b109d09e4dec04560948b
Jens Van de Voorde <jens.van.de.voorde@student.ehb.be>
2019-05-21 15:14:06 +0200

    Navigatie af + nog probleem met add en delete button

--- fixing share integration/jQuery/FixedShare.js
+++ fixing share integration/jQuery/FixedShare.js
@@ -1,17 +1,43 @@
 $(document).ready(function(){
     console.log("script linked");
+    $(".sharedWithYou").hide();
+    $(".recentlyUsed").hide();
+    $(".muchUsed").hide();
     $(".newStoryboards").hide();
+    $(".Gedeeld button").click(gedeeldMetJou);
+    $(".Recente button").click(recent);
+    $(".Veel-gebruikte button").click(veelGebruikt);
+    $(".Van-u-gedeeld button").click(gedeeldMetAnderen);
     $(".uitklap").click(function(){
         $(this).toggleClass("animate-listitem");
+    });
+
+    function gedeeldMetJou(){
+        $(".sharedWithYou").slideToggle();
+    }
+    function recent(){
+         $(".recentlyUsed").slideToggle();
+    }
+    function veelGebruikt(){
+         $(".muchUsed").slideToggle();
+       
+    }
+
+    function gedeeldMetAnderen(){
         $(".newStoryboards").slideToggle();
-        $("#plus").on("click", function(){
-            $(".newStoryboards").append("<img src='Afbeeldingen/ShareIcon.jpg' alt='otherTest' id='test'>");   
-        });
-        $("#minus").on("click", function(){
+        $("#plus").on("click", add);
+        function add(){
+            $(".newStoryboards").append("<div id='testSpecial'><a><Strong>CP2</Strong></a><a>Introduction</a><a>15/03</a></div>");  
+            console.log(add);
+        }
+        
+        $("#minus").on("click", remove);
+        function remove(){
             function removeElement(storyboard) {
                 storyboard.parentNode.removeChild(storyboard);
             }
-            removeElement(document.getElementById("test"));
-        });
-      });
-    });
+            removeElement(document.getElementById("testSpecial"));
+            console.log(remove);
+        }
+    }
+});
