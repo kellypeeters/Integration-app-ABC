80477f696ff297ea28382179bac4050919a1d0ac
KellyPeeters <kelly.peeters@student.ehb.be>
2019-05-23 14:53:15 +0200

    Mappen geordend

--- Jquery/LocalStorage.js
+++ Jquery/LocalStorage.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-    var $button = $('.add-item');
-    var $input = $('.input-field1');
-    var $input1 = $('.input-field2');
-    var $input2 = $('.input-field3');
-    var $input3 = $('.input-field4');
+    var button = $('.add-item');
+    var input = $('.input-field1');
+    var input1 = $('.input-field2');
+    var input2 = $('.input-field3');
+    var input3 = $('.input-field4');
 
-    var $list = $('.list');
+    var list = $('.list');
     if (localStorage.getItem('user-input')) {
         $list.html(localStorage.getItem('user-input'));
     }
    
     $button.on('click', function () {
 
-        $list.append('<li>' + $input.val() + $input1.val() + $input2.val() + $input3.val() ); 
+        $list.append('<li>' + input.val() + input1.val() + input2.val() + input3.val() ); 
         
         console.log('.list');
 
-        localStorage.setItem('user-input', $list.html());
+        localStorage.setItem('user-input', );
 
         $input.val('');
 
@@ -24,6 +24,6 @@ $(document).ready(function () {
 
     $('.Aanmeldbutton').on('click', function () {
         console.log('update');
-        localStorage.setItem('user-input', $list.html());
+        localStorage.setItem('user-input', list.html());
     });
 });  
