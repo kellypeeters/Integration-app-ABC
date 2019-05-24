23a9ad15a3102885a314e6a2412c2b79f982695e
KellyPeeters <kelly.peeters@student.ehb.be>
2019-05-24 10:01:08 +0200

    Local storage fixed

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,33 +1,15 @@
 console.log(localStorage);
 const inpKey = document.getElementById("inpKey");
 const inpValue = document.getElementById("inpValue");
-const password = document.getElementById("password");
-const cpassword = document.getElementById("cpassword");
+const Wachtwoord = document.getElementById("Wachtwoord");
+const BevestigWachtwoord = document.getElementById("BevestigWachtwoord");
 
-const btnInsert = document.getElementById("btnInsert");
-const IsOutput = document.getElementById("IsOutput");
-
-btnInsert.onclick = function () {
-    const key = inpKey.value;
-    const value = inpValue.value;
-    const wachtwoord = password.value;
-    const cwachtwoord = cpassword.value;
-    console.log(key);
-    console.log(value);
-    console.log(wachtwoord);
-    console.log(cwachtwoord);
-
-    if (key && value && wachtwoord && cwachtwoord) {
-        localStorage.setItem('Naam', key);
-        localStorage.setItem('E-mail', value);
-        localStorage.setItem('Wachtwoord', password);
-        localStorage.setItem('Bevestig wachtwoord', cpassword); 
-    }
-};
-
-for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);  
-
-    IsOutput.innerHTML += `${key}: ${value}: ${wachtwoord}: ${cwachtwoord}<br/>`; 
-}
+$("#registrationForm").submit(function (event) {
+    var formData = $('#registrationForm').serializeArray();
+    event.preventDefault();
+    console.log(formData);
+    localStorage.setItem('Naam', inpKey.value);
+    localStorage.setItem('E-mail', inpValue.value);
+    localStorage.setItem('Wachtwoord', Wachtwoord.value);
+    localStorage.setItem('Bevestig wachtwoord', BevestigWachtwoord.value); 
+});     
