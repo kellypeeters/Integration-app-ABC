53cf0a36a8dc6353b237a7e2407b66ff84e625dc
KellyPeeters <kelly.peeters@student.ehb.be>
2019-05-24 12:48:31 +0200

    Vanalles

--- Jquery/PersoonlijkePagina.js
+++ Jquery/PersoonlijkePagina.js
@@ -1,37 +1,34 @@
-$(".image-box").click(function(event) {
-	var previewImg = $(this).children("img");
+$(".image-box").click(function (event) {
+    var previewImg = $(this).children("img");
 
-	$(this)
-		.siblings()
-		.children("input")
-		.trigger("click");
+    $(this)
+        .siblings()
+        .children("input")
+        .trigger("click");
 
-	$(this)
-		.siblings()
-		.children("input")
-		.change(function() {
-			var reader = new FileReader();
+    $(this)
+        .siblings()
+        .children("input")
+        .change(function () {
+            var reader = new FileReader();
 
-			reader.onload = function(e) {
-				var urll = e.target.result;
-				$(previewImg).attr("src", urll);
-				previewImg.parent().css("background", "transparent");
-				previewImg.show();
-				previewImg.siblings("p").hide();
-			};
-			reader.readAsDataURL(this.files[0]);
-		});
+            reader.onload = function (e) {
+                var urll = e.target.result;
+                $(previewImg).attr("src", urll);
+                previewImg.parent().css("background", "transparent");
+                previewImg.show();
+                previewImg.siblings("p").hide();
+            };
+            reader.readAsDataURL(this.files[0]);
+        });
 });
 
 //Overgenomen van Codepen.io   
 
-    function next(e){
-        e.preventDefault();
-        var getNaam = localStorage.getItem('Naam');
-        var getEmail = localStorage.getItem('E-mail');
-        var setTitel = document.getElementById("input_titel");
-        var setDeadline = document.getElementById("input_deadline");
-         
-        setTitel.innerHTML = getNaam.value;
-        setDeadline.innerHTML = getEmail.value;
-    }  
+function next(e) {  
+    e.preventDefault();
+    
+    document.getElementById("Voornaam").innerHTML = localStorage.getItem('Naam');
+    document.getElementById("Email").innerHTML = localStorage.getItem('E-mail');
+}   
+});
